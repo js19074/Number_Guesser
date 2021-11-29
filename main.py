33a750bf80b3094810e15d0466c245bb69275354
Jasnoor Singh <78879596+js19074@users.noreply.github.com>
2021-11-29 06:29:54 +0000

    I made sure that everytime I guess a number, the number stays the same. This way I can actually guess the number.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Number Guesser - Version 1
+#Number Guesser - Version 2
 #29 Nov 2021
 #Jasnoor
 
@@ -6,18 +6,33 @@ import random
 
 #Target variable to store a random target number for the user to guess
 target = random.randint(1, 100)
-#Guess variable to store the user's guess number
-guess = int(input("Please guess the number: "))
+
+#Controls whether while loop runs. Repeat until user guesses correct number
+run_loop = True
+
+while run_loop:
+
+    #Guess variable to store the user's guess number
+    guess = int(input("Please guess the number (1-100): "))
+
+    #Repeats until user inputs number between 1-100
+    while not(guess >= 1 and guess <=100):
+        guess = int(input("That is not a valid number. Please guess between 1 and 100: "))
+    
+    if guess < target:  #Checking whether the user's guess is less than the target
+        print("Your guess was too low")
+    elif guess > target:  #Checking whether the user's guess is greater than the target
+        print("Your guess was too high")
+    elif guess == target: #Check for correct guess
+        #Stop repeating loop
+        run_loop = False
+
+    
+
+#When the user has guessed the correct number
+print("Congratulations! You got the number")
 
 
-if guess == target:   #Checking whether the user's guess is the same as the target
-    print("Congratulations! You got the number")
-elif guess < target:  #Checking whether the user's guess is less than the target
-    print("Your guess was too low")
-elif guess > target:  #Checking whether the user's guess is greater than the target
-    print("Your guess was too high")
-else:                 #In case something goes wrong - shouldn't actually run
-    print("Something went wrong")
 
 
 
